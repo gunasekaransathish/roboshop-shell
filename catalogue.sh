7c9ea814441fbc16c657d8ebbc6f66f15e56574a
sathishgunasekaran <sathish.gunasekaran@striim.com>
2024-10-12 21:36:14 +0530

    added mongodb and catalogue

---
+++ catalogue.sh
@@ -0,0 +1,27 @@
+cp catalogue.service /etc/systemd/system/catalogue.service
+cp mongodb.repo /etc/yum.repos.d/mongodb.repo
+
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+cd /app
+unzip /tmp/catalogue.zip
+
+cd /app
+npm install
+
+cp catalogue.service /etc/systemd/system/catalogue.service
+
+dnf install mongodb-mongosh -y
+mongosh --host MONGODB-SERVER-IPADDRESS </app/db/master-data.js
+
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
+
---
+++ mongodb.sh
@@ -0,0 +1,8 @@
+dnf install mongodb-org -y
+
+cp mongodb.conf /etc/yum.repos.d/mongo.repo
+
+sed -i -e `/s/127.0.0.1/0.0.0.0` /etc/mongod.conf
+systemctl enable mongod
+systemctl start mongod
+
